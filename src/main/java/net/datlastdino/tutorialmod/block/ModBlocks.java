4357eaf3609e668a42888ee265074c01ba972607
datlastdino <lucasemillarsen@gmail.com>
2025-03-07 10:50:42 +0100

    advansed block

--- src/main/java/net/datlastdino/tutorialmod/block/ModBlocks.java
+++ src/main/java/net/datlastdino/tutorialmod/block/ModBlocks.java
@@ -1,6 +1,7 @@
 package net.datlastdino.tutorialmod.block;
 
 import net.datlastdino.tutorialmod.TutorialMod;
+import net.datlastdino.tutorialmod.block.custom.MagicBlock;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
@@ -32,6 +33,8 @@ public class ModBlocks {
     public static final Block PINK_GARNET_DEEPSLATE_ORE = registerBlock("pink_garnet_deepslate_ore",
             new ExperienceDroppingBlock(UniformIntProvider.create(3, 6),
                     AbstractBlock.Settings.create().strength(4f).requiresTool().sounds(BlockSoundGroup.DEEPSLATE)));
+    public static final Block MAGIC_BLOCK =registerBlock("magic_block",
+            new MagicBlock(AbstractBlock.Settings.create().strength(1f).requiresTool()));
 
     private static Block registerBlock(String name, Block block) {
         registerBlockItem(name, block);
