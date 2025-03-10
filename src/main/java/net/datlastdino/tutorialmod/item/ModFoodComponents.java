03e6a3cf92b7068f7d62506312539812157ee8e2
datlastdino <lucasemillarsen@gmail.com>
2025-03-10 10:48:56 +0100

    fuel and food

---
+++ src/main/java/net/datlastdino/tutorialmod/item/ModFoodComponents.java
@@ -0,0 +1,10 @@
+package net.datlastdino.tutorialmod.item;
+
+import net.minecraft.component.type.FoodComponent;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
+
+public class ModFoodComponents {
+    public static final FoodComponent CAULIFLOWER = new FoodComponent.Builder().nutrition(3).saturationModifier(0.25f)
+            .statusEffect(new StatusEffectInstance(StatusEffects.INSTANT_HEALTH, 20),0).build();
+}
--- src/main/java/net/datlastdino/tutorialmod/item/ModItems.java
+++ src/main/java/net/datlastdino/tutorialmod/item/ModItems.java
@@ -2,10 +2,7 @@ package net.datlastdino.tutorialmod.item;
 
 import net.datlastdino.tutorialmod.TutorialMod;
 import net.datlastdino.tutorialmod.item.custom.ChiselItem;
-import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroup;
-import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
@@ -16,6 +13,8 @@ public class ModItems {
 
     public static final Item CHISEL = registerItem("chisel", new ChiselItem(new Item.Settings().maxDamage(32)));
 
+    public static final Item CAULIFLOWER = registerItem("cauliflower", new Item(new Item.Settings().food(ModFoodComponents.CAULIFLOWER)));
+    public static final Item STARLIGHT_DUST = registerItem("starlight_dust", new Item(new Item.Settings()));
     private static Item registerItem(String name, Item item) {
         return Registry.register(Registries.ITEM, Identifier.of(TutorialMod.MOD_ID, name), item);
     }
