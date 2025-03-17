e26f081c51613c9d0f8a28e69ba1c5ece94e6af6
datlastdino <lucasemillarsen@gmail.com>
2025-03-17 09:27:15 +0100

    datagen

--- src/main/java/net/datlastdino/tutorialmod/TutorialModDataGenerator.java
+++ src/main/java/net/datlastdino/tutorialmod/TutorialModDataGenerator.java
@@ -1,11 +1,20 @@
 package net.datlastdino.tutorialmod;
 
+import net.datlastdino.tutorialmod.datagen.ModBlockTagProvider;
+import net.datlastdino.tutorialmod.datagen.ModItemTagProvider;
+import net.datlastdino.tutorialmod.datagen.ModLootTableProvider;
+import net.datlastdino.tutorialmod.datagen.ModModelProvider;
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 
 public class TutorialModDataGenerator implements DataGeneratorEntrypoint {
 	@Override
 	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+		FabricDataGenerator.Pack pack =fabricDataGenerator.createPack();
 
+		pack.addProvider(ModBlockTagProvider::new);
+		pack.addProvider(ModItemTagProvider::new);
+		pack.addProvider(ModLootTableProvider::new);
+		pack.addProvider(ModModelProvider::new);
 	}
 }
